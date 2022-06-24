2caeb472660b154e89b2c2554a1a646513e1e95d
webtechromanov <66905718+webtechromanov@users.noreply.github.com>
2022-06-24 15:05:56 +0300

    Update HealthBar.cs

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -9,6 +9,9 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private Player _player;
     [SerializeField] private Slider _slider;
 
+    private float MoveSpeed = 2f;
+    private Coroutine _coroutine;
+
     private void OnEnable()
     {
         _player.HealthChanged += OnHealthChanged;
@@ -22,6 +25,19 @@ public class HealthBar : MonoBehaviour
 
     private void OnHealthChanged(float value)
     {
-        _slider.DOValue(value / _player.MaxHealth, 1f);
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        _coroutine = StartCoroutine(ChangeValue(value));
+    }
+
+    public IEnumerator ChangeValue(float value)
+    {
+        float health = value / _player.MaxHealth;
+        while(_slider.value != health)
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, health, MoveSpeed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
